e092c2cb871f1edbc71348a577eb1c588672413f
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-09 19:29:34 +0300

    Add files via upload
    D4

---
+++ skibidus4.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    string   s;
+    cin >> s;
+    s.replace(s.size() - 2, 2, "i");
+    cout << s << endl;
+    }
+    return 0;
+
+  }
+  
+
+  
